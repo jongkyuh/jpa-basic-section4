27bb86bba330386c6ea734935b2fd4571a59b04f
jongkyuh <jongkyuh@naver.com>
2024-08-06 22:53:26 +0900

    3. 연관관계 매핑
    참조 키를 직접 필드로 만드는 것이 아니라 객체를 만들어 ORM을 준수 할 수 있게 한다. 그리고 양방향, 단방향 매핑에 대해서도 공부했는데, 자세한 코드는 노션에 있다.

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 
 public class JpaMain {
 
@@ -21,11 +22,46 @@ public class JpaMain {
         tx.begin();
 
         //code.....
+        Member member1 = null;
         try {
-            Member member = new Member();
-            member.setName("함종규");
-            em.persist(member);
-            // 커밋시점에 반영
+            member1 = new Member();
+            member1.setName("A");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setName("B");
+            em.persist(member2);
+
+            Order order1 = new Order();
+            order1.setMember(member1);
+            member1.getOrders().add(order1);
+            em.persist(order1);
+
+            Order order2 = new Order();
+            order2.setMember(member1);
+            member1.getOrders().add(order2);
+            em.persist(order2);
+
+            Order order3 = new Order();
+            order3.setMember(member1);
+            em.persist(order3);
+
+            order3.setMember(member2);
+            member2.getOrders().add(order3);
+
+
+            for (Order order : member1.getOrders()) {
+                System.out.println("order = " + order.getId());
+            }
+
+
+            System.out.println("+++++++++++++++++++++++++");
+//            em.clear();
+
+            for (Order order : member1.getOrders()) {
+                System.out.println("order = " + order);
+            }
+            System.out.println("++++++++++++++++++++");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -33,6 +69,15 @@ public class JpaMain {
             em.close();
 
         }
+        EntityManager em2 = emf.createEntityManager();
+        EntityTransaction transaction2 = em2.getTransaction();
+        transaction2.begin();
+        Member findMember = em2.find(Member.class, member1.getId());
+        for (Order order : findMember.getOrders()) {
+            System.out.println("order = " + order);
+        }
+        transaction2.commit();
+        em2.close();
         // 6. 전체 어플리케이션이 종료하면 엔티티매니저팩토리 닥기
         emf.close();
 
