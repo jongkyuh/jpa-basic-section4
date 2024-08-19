419ebbca39f3b648879d920ffc7a66ccb9a6c853
jongkyuh <jongkyuh@naver.com>
2024-08-19 20:35:11 +0900

    7. 값 타입

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -9,13 +9,13 @@ public class Delivery {
     @Column(name = "DELIVERY_ID")
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+   private Address address;
 
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY)
     private Order order;
+
 
 }
